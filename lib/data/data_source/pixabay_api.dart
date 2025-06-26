243e049ab3b665b188e46446f323fe7b68fb578d
Your Namepaxfortuna <avidia38@gmail.com>
2025-06-26 10:58:36 +0900

    List<Mpa> to Iterable

--- lib/data/data_source/pixabay_api.dart
+++ lib/data/data_source/pixabay_api.dart
@@ -9,7 +9,19 @@ class PixabayApi {
   static final apiKey = dotenv.env['API_KEY'] ?? '';
   static const baseUrl = 'https://pixabay.com';
 
-  Future<List<Map<String, dynamic>>> fetch(String query) async {
+  // Future<List<Map<String, dynamic>>> fetch(String query) async {
+  //   final response = await client.get(
+  //     Uri.parse(
+  //       '$baseUrl/api/?key=$apiKey&q=$query&image_type=photo&pretty=true'
+  //     ),
+  //   );
+  //   Map<String, dynamic> jsonResponse = jsonDecode(response.body);
+  //   List<Map<String, dynamic>> hits = List<Map<String, dynamic>>.from(jsonResponse['hits']);
+
+  //   return hits;
+  // }
+  // Iterable 타입으로 대체
+  Future<Iterable> fetch(String query) async {
     final response = await client.get(
       Uri.parse(
         '$baseUrl/api/?key=$apiKey&q=$query&image_type=photo&pretty=true'
