db23de3283d28f89c6d7607b8fa5f05c744938c0
Your Namepaxfortuna <avidia38@gmail.com>
2025-07-01 16:56:29 +0900

    remove: unused get

--- lib/user/presentation/detail/user_detail_page.dart
+++ lib/user/presentation/detail/user_detail_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
 import 'package:image_search/a_common/components/app_font.dart';
 import 'package:image_search/user/domain/model/user.dart';
 
